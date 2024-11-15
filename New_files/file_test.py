4c6a56a8859978781a04c939dfb84f9faddbf3ab
fuzzycroc <152718375+fuzzycroc@users.noreply.github.com>
2024-11-15 17:43:52 +0100

    KTH work

---
+++ New_files/file_test.py
@@ -0,0 +1,9 @@
+with open("\\ug.kth.se\dfs\home\p\e\peries\appdata\xp.V2\Documents\GitHub\Test_scripts\New_files\number.txt", "r") as number_file:
+    print(number_file.read())
+
+
+#path files work when you modify the path file from 
+# \\ug.kth.se\dfs\home\p\e\peries\appdata\xp.V2\Documents\GitHub\Test_scripts\New_files\number.txt
+# to 
+
+# r"\\ug.kth.se\dfs\home\p\e\peries\appdata\xp.V2\Documents\GitHub\Test_scripts\New_files\number.txt"
