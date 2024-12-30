b3ed3540daa0bc93c57194d5155261ad4635d355
fuzzycroc <152718375+fuzzycroc@users.noreply.github.com>
2024-12-30 13:22:16 +0100

    update

--- New_files/file_test.py
+++ New_files/file_test.py
@@ -1,9 +1,12 @@
-with open("\\ug.kth.se\dfs\home\p\e\peries\appdata\xp.V2\Documents\GitHub\Test_scripts\New_files\number.txt", "r") as number_file:
-    print(number_file.read())
 
 
-#path files work when you modify the path file from 
-# \\ug.kth.se\dfs\home\p\e\peries\appdata\xp.V2\Documents\GitHub\Test_scripts\New_files\number.txt
-# to 
+matrix = []
 
-# r"\\ug.kth.se\dfs\home\p\e\peries\appdata\xp.V2\Documents\GitHub\Test_scripts\New_files\number.txt"
+filename = open(r"\\ug.kth.se\dfs\home\p\e\peries\appdata\xp.V2\Documents\GitHub\Test_scripts\New_files\trail_number.txt", "r")  #this opens it for reading mode 
+
+for lines in range(-1,10): 
+
+    test = filename.readline()
+    matrix.append(test)
+
+    print (matrix)
